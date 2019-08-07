16a52c3e94c182bcc7b7cdb06122b69b56e4209c
John Karrell Speck <speckworks@gmail.com>
2019-08-07 18:49:04 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -38,7 +38,6 @@ end
 def square_root(num)
   describe "#square_root" do
   it "returns the square root of a number" do
-    expect(square_root(num**2)).to eq(num)
-  square_root**2 = num
+  square_root = Math.sqrt(n)
   puts square_root 
 end
